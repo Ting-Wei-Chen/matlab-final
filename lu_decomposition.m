f8deff358770cd0aa2efc5df9763506d58f3d115
Ting-Wei-Chen <51171682+Ting-Wei-Chen@users.noreply.github.com>
2019-06-13 18:11:03 +0800

    add lu decomposition

---
+++ lu_decomposition.m
@@ -0,0 +1,27 @@
+function [L,U,P]=lu_decomposition(old)
+[row,col]=size(old);
+L=eye(row);
+P=eye(row);
+
+for i=1:1:row
+    if old(i,1) ~=0
+        old=row_exchange(old,1,i);
+        P=row_exchange(P,1,i);
+        break
+    end
+end
+
+for i=1:1:row-1
+    for j=1:1:col
+        if old(i,j)~=0
+            for k=i+1:1:row
+                temp=old(k,j)/old(i,j);
+                old=add(old,i,k,-temp);
+                L=add(L,i,k,temp);
+            end
+            break
+        end
+    end
+end
+U=old;
+end
